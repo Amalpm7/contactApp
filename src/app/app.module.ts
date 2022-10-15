4eee38fa4240caa11193324e7625c2ce2d183761
Amalpm7 <amal.p@nestgroup.net>
2022-10-15 11:52:29 +0530

    contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewContactsComponent } from './view-contacts/view-contacts.component';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:ViewContactsComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
